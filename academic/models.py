48eebc0eb11f56002238a885a37a986d38307d91
Mestu Paul <paulmestu@gmail.com>
2023-12-25 12:41:58 +0600

    add new department

--- academic/models.py
+++ academic/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
 class Session(models.Model):
     department = models.ForeignKey('Department', on_delete=models.CASCADE)
     session_name = models.CharField(max_length=100)
 
 class Department(models.Model):
-    department = models.CharField(max_length=100, null=True)
+    department = models.CharField(max_length=100, null=True, unique=True)
 
 class Syllabus(models.Model):
     syllabus = models.ForeignKey('Session', on_delete=models.CASCADE)
