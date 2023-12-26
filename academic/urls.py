00ec47df85c09a2825d8ff8e8d4c9c964183609c
Bonusree <bonusreedatta820@gmail.com>
2023-12-26 13:53:28 +0600

    delete of syllabus, rutine, question

--- academic/urls.py
+++ academic/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('academic/', views.academic, name='academic'),
-    path('add_syllabus/', views.add_syllabus, name='add_syllabus'),
+    path('academic/add_syllabus/', views.add_syllabus, name='add_syllabus'),
+    path('academic/delete_syllabus', views.delete_syllabus, name='delete_syllabus'),
+    path('routine/delete_routine', views.delete_routine, name='delete_routine'),
+    path('question_bank/delete_question_bank', views.delete_question, name='delete_question'),
     path('add_routine/', views.add_routine, name='add_routine'),
     path('add_question/', views.add_question, name='add_question'),
     path('routine/', views.routine, name='routine'),
